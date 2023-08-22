c354fb9204ffb3b11674cc2b92f09b6431d30f5e
Rishab Innani <110304592+RishabInanni@users.noreply.github.com>
2023-08-22 11:46:33 +0530

    Create Logindatas.sql
    Rishab Innani :  created the SQL server

---
+++ Backend/Sql/Logindatas.sql
@@ -0,0 +1,31 @@
+/* login o the admin */
+
+/* 1. verification */
+Select * from admin where userid = ? and password = ? ;
+
+/* after verification */
+Select * from admin where userid = ? ;
+
+/*  to add student from admin panel */
+
+
+/* to add teacher from the admin panel */
+
+
+
+
+
+/* Student login */
+
+
+
+
+/* Teacher Login */
+
+
+
+
+
+
+
+
