cd89ba866f28aee9ba76fbd2776b15996616d821
Rishab Innani <110304592+RishabInanni@users.noreply.github.com>
2023-08-23 21:18:51 +0530

    updated query
    table admin added

--- Backend/Sql/Logindatas.sql
+++ Backend/Sql/Logindatas.sql
@@ -1,10 +1,32 @@
-/* login o the admin */
+/*  admin table*/
+create table admin(
+admin_id BIGINT,
+admin_password varchar(20) not null,
+	admin_email varchar(50)not null,
+	admin_address varchar(50) not null,
+	admin_image varchar(50) not null,
+	
+	primary key(admin_id)
+);
+/* login to the admin */
 
 /* 1. verification */
-Select * from admin where userid = ? and password = ? ;
+Select * from admin where admin_id = ? and admin_password = ? ;
 
 /* after verification */
-Select * from admin where userid = ? ;
+Select * from admin where admin_id = ? ;
+
+
+/* update admin password */
+alter table admin
+set password=? where admin_id=? ;
+
+/* update image */
+alter table admin
+update image=? where admin_id=? ;
+/*  */
+
+
 
 /*  to add student from admin panel */
 
