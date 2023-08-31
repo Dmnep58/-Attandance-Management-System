9f21a914e11cc89169be592f2922669a4ad3f174
Dmnep58 <dpm90797@gmail.com>
2023-08-31 15:28:33 +0530

    Required Queries added
    Required Queried queries created

--- Backend/Sql/registration.sql
+++ Backend/Sql/registration.sql
@@ -1,8 +1,21 @@
 -- view all details in registration table
 SELECT * FROM registration;
 
+
+--truncate the table
+TRUNCATE table registration;
+
 -- data insetion in the table
 INSERT into registration values(? ,? , ? , ? , ? , ? , ? , ?);
+
+
+-- fetch data according to role
+SELECT * FROM registration where role = 'Teacher';
+SELECT * FROM registration where role = 'student';
+
+-- fetch no of people on the basis of the role
+select count(*) from registration where role='student';
+select count(*) from registration where role='Teacher';
 
 /* 1. verification */
 Select * from registration where uid = ? and password = ? ;
@@ -16,6 +29,9 @@ insert into registration values()
 UPDATE registration
 SET password = ? WHERE uid = ?;
 
+
+-- update columns
+alter table registration rename column roll to role;
 
 /* update image */
 UPDATE registration
