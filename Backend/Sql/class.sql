00926411217c05f52bfae32aeb135020fc55a972
Dmnep58 <dpm90797@gmail.com>
2023-09-06 17:45:30 +0530

    Simple correction in sql queries

--- Backend/Sql/class.sql
+++ Backend/Sql/class.sql
@@ -9,7 +9,7 @@ TRUNCATE table class;
 INSERT into class values(? ,? , ? , ? , ? , ? , ? , ?, ?);
 
 --add row in class
-INSERT into class values(1,'Java',51124,1,'1.png','09:00:00','10:00:00');
+INSERT into class values(1,'Java',51124,2,'1.png','09:00:00','10:00:00');
 
 
 -- fetch data according to class name
--- Backend/Sql/enrollment.sql
+++ Backend/Sql/enrollment.sql
@@ -1,5 +1,8 @@
 /* displaying all record */
-select * from enrollment;
+select * from facultyenrollment;
 
 -- Insert into student enrollment table
 Insert into studentenrollment(class_id,student_id,batch) values (1,1908,'Java');
+
+--insert into facultyenrollment
+INSERT into facultyenrollment VALUES(1,51124);
