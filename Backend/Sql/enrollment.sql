fa5a5035c33d954c13374c199cdf5c3798428c96
Dmnep58 <dpm90797@gmail.com>
2023-09-07 17:10:33 +0530

    Update enrollment.sql

--- Backend/Sql/enrollment.sql
+++ Backend/Sql/enrollment.sql
@@ -2,7 +2,7 @@
 select * from facultyenrollment;
 
 -- Insert into student enrollment table
-Insert into studentenrollment(class_id,student_id,batch) values (1,1908,'Java');
+Insert into studentenrollment(class_id,student_id,batch) values (1,1908,'Java1');
 
 --insert into facultyenrollment
 INSERT into facultyenrollment VALUES(1,51124);
