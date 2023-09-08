0adc7fe453170f735ad851d45c102f6f55883492
Santosh Kumar Sah <142821967+santosh-333@users.noreply.github.com>
2023-09-08 16:58:05 +0530

    Update attendance.sql
    some queries updated in attendance table

--- Backend/Sql/attendance.sql
+++ Backend/Sql/attendance.sql
@@ -9,15 +9,17 @@ VALUES(1908,75823,1,2,'Present','2023-09-06');
 -- Fetch Attendance data according date 
 SELECT * from attendance where date=?;
 
+--Fetch attendance data according faculty id
+select * from attendance where teacher_id = ?
 
 --Fetch Attendence data according faculty and batch 
-SELECT a.*
-FROM attendance a
-INNER JOIN studentenrollment se ON a.student_id = se.student_id
-INNER JOIN facultyenrollment fe ON se.course_id = fe.course_id
-WHERE fe.teacher_id = ?
-  AND fe.batch = ?;
-
+SELECT a.* FROM attendance a 
+INNER JOIN studentenrollment se 
+ON a.student_id = se.student_id 
+INNER JOIN facultyenrollment fe 
+ON se.course_id = fe.course_id 
+WHERE fe.teacher_id = ? 
+AND fe.batch = ? AND se.batch = ?
 
 -- Fetch Attendance data according to batch and date
 SELECT a.*, se.batch
