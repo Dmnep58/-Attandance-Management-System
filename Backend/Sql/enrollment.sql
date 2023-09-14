f0ee58e9371e6aa829f57a77a2a7fa705a1771fa
Dmnep58 <dpm90797@gmail.com>
2023-09-14 15:39:36 +0530

    Update enrollment.sql
    update some mistakes in query

--- Backend/Sql/enrollment.sql
+++ Backend/Sql/enrollment.sql
@@ -18,10 +18,10 @@ Insert into facultyenrollment(class_id,student_id,batch) values (1,1908,'Java1')
 INSERT into facultyenrollment VALUES(1,51124);
 
 --Total number of teacher enrolled in batch
-select count(distinct(batch)) from facultyenrollment  where teacher_id=?
+select count(distinct(batch)) from facultyenrollment where teacher_id=20051958;
 
 --total number of students enrolled till now
 select count(*) from registration where role='student'
 
 --Fetch number of courses enrolled by the teacher
-select count(distinct(course_id)) from facultyenrollment  where teacher_id=?
+select count(distinct(course_id)) from facultyenrollment  where teacher_id=20051958;
