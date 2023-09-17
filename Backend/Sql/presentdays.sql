363528b07b3bf64cbf7d8dee42a9577a93276dbd
Santosh Kumar Sah <142821967+santosh-333@users.noreply.github.com>
2023-09-17 15:44:27 +0530

    Update presentdays.sql
    updated queries

--- Backend/Sql/presentdays.sql
+++ Backend/Sql/presentdays.sql
@@ -12,17 +12,21 @@
 -- WHERE a.student_id = ? AND a.teacher_id = ? AND f.batch = ?;
 
 -- Update totaldays for all rows where teacher_id and batch match
-UPDATE ATTENDANCE AS a
-INNER JOIN facultyenrollment AS f ON a.teacher_id = f.teacher_id
-SET a.totaldays = a.totaldays + 1
-WHERE a.teacher_id = ? AND f.batch = ?;
+UPDATE ATTENDANCE AS a 
+INNER JOIN facultyenrollment AS f 
+ON a.teacher_id = f.teacher_id 
+SET a.total_days = a.total_days + 1 
+WHERE a.student_id = ? 
+AND a.teacher_id = ? AND f.batch = ?
 
 
 -- Update present days for the given student_id, teacher_id, and batch
-UPDATE ATTENDANCE AS a
-INNER JOIN facultyenrollment AS f ON a.teacher_id = f.teacher_id
-SET a.present = a.present + 1
-WHERE a.student_id = ? AND a.teacher_id = ? AND f.batch = ?;
+UPDATE ATTENDANCE AS a 
+INNER JOIN facultyenrollment AS f 
+ON a.teacher_id = f.teacher_id 
+SET a.present_days = a.present_days + 1, a.total_days = a.total_days + 1 
+WHERE a.student_id = ? AND a.teacher_id = ? 
+AND f.batch = ?
 
 
 
