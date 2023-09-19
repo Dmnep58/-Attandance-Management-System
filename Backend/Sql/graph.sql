1a85ad1dffc49a5775ead7f9e98ff15c15158b44
Rishab Innani <110304592+RishabInanni@users.noreply.github.com>
2023-09-19 16:18:35 +0530

    Update graph.sql
    updated

--- Backend/Sql/graph.sql
+++ Backend/Sql/graph.sql
@@ -1,10 +1,12 @@
 
 --top 3 course
-SELECT course_id, COUNT(student_id) AS student_count
-FROM studentenrollment
-GROUP BY course_id
+SELECT c.course_name, COUNT(se.student_id) AS student_count
+FROM course c
+JOIN studentenrollment se ON c.course_id = se.course_id
+GROUP BY c.course_name
 ORDER BY student_count DESC
 LIMIT 3;
+
 
 
 --top 3 batch
